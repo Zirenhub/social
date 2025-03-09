efc3e1b6e77e6beb20c75f0aff49fe50f82c52b0
Erdinch Osman <erdinchorhan02@gmail.com>
2025-03-09 04:12:11 +0200

    types

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,24 +1,21 @@
 import { Prisma } from '@prisma/client';
+import Profile from '@/types/profile';
 
-export interface ApiResponse<T> {
-  success: boolean;
-  data: T | null;
-  error:
-    | {
-        message: string;
-      }
-    | Record<
-        string,
-        {
-          message: string;
-        }
-      >
-    | null;
-}
+export type ApiResponse<T> =
+  | { success: true; data: T; error: null }
+  | { success: false; data: null; error: ApiError };
+
+// Unified error type
+export type ApiError = {
+  message: string;
+  fields?: Record<string, string>; // Optional field-specific errors
+};
 
 export type SessionUser = Prisma.UserGetPayload<{
   include: {
     profile: true;
   };
   omit: { hashedPassword: true };
-}>;
+}> & {
+  profile: NonNullable<Profile>;
+};
--- src/types/auth.ts
+++ src/types/auth.ts
@@ -11,39 +11,47 @@ function getMaxCharError(field: string, maxChar: number) {
 export const SignUpContent = z
   .object({
     username: z
-      .string()
+      .string({ message: 'Username must be included.' })
       .trim()
       .min(3, getMinCharError('Username', 3))
       .max(14, getMaxCharError('Username', 14)),
     firstName: z
-      .string()
+      .string({ message: 'First Name must be included.' })
       .trim()
       .min(3, getMinCharError('First Name', 3))
       .max(14, getMaxCharError('First Name', 14)),
     lastName: z
-      .string()
+      .string({ message: 'Last Name must be included.' })
       .trim()
       .min(3, getMinCharError('Last Name', 3))
       .max(14, getMaxCharError('Last Name', 14)),
-    day: z.number().min(1, 'Day is required').max(31),
-    month: z.number().min(0, 'Month is required').max(11),
+    day: z
+      .number({ message: 'Invalid day.' })
+      .min(1, 'Day is required')
+      .max(31),
+    month: z
+      .number({ message: 'Invalid month.' })
+      .min(0, 'Month is required')
+      .max(11),
     year: z
-      .number()
+      .number({ message: 'Invalid year.' })
       .min(1900, 'Year is required')
       .max(new Date().getFullYear()),
     gender: z.enum(['MALE', 'FEMALE', 'OTHER'], { message: 'Invalid gender.' }),
     bio: z.string().trim().max(160, getMaxCharError('Bio', 160)).optional(),
     email: z
-      .string()
+      .string({ message: 'Email must be included.' })
       .trim()
       .max(32, { message: "Email address can't be longer than 32 characters" })
       .email({ message: 'Invalid email address.' }),
     password: z
-      .string()
+      .string({ message: 'Password must be included.' })
       .trim()
       .min(3, getMinCharError('Password', 3))
       .max(18, getMaxCharError('Password', 18)),
-    confirmPassword: z.string(),
+    confirmPassword: z.string({
+      message: 'Confirm Password must be included.',
+    }),
   })
   .superRefine(({ confirmPassword, password }, ctx) => {
     if (confirmPassword !== password) {
@@ -57,12 +65,12 @@ export const SignUpContent = z
 
 export const LogInContent = z.object({
   email: z
-    .string()
+    .string({ message: 'Email must be included.' })
     .trim()
     .max(32, { message: "Email address can't be longer than 32 characters" })
     .email({ message: 'Invalid email address.' }),
   password: z
-    .string()
+    .string({ message: 'Password must be included.' })
     .trim()
     .min(3, getMinCharError('Password', 3))
     .max(18, getMaxCharError('Password', 18)),
