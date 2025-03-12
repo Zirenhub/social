30c986f78af1a2f790c3740bd7b74e0bcc0d7260
Erdinch Osman <erdinchorhan02@gmail.com>
2025-03-12 02:53:44 +0200

    delete post added and improved create post

--- src/app/api/post/actions.ts
+++ src/app/api/post/actions.ts
@@ -2,21 +2,40 @@
 import { prisma } from '@/lib/prisma';
 import successResponse, { errorResponse } from '../response';
 import { PostContent } from '@/types/post';
-import { getSession } from '@/lib/session';
+import { revalidateTag } from 'next/cache';
+import { getUser } from '@/lib/session';
 
 export async function createPost({ content }: { content: string }) {
   try {
-    const user = await getSession();
-    if (!user.success || !user.data) {
-      throw new Error('User could not be verified.');
-    }
+    const user = await getUser();
     const parsed = PostContent.parse({ content });
     const post = await prisma.post.create({
-      data: { content: parsed.content, profileId: user.data.profile.id },
+      data: { content: parsed.content, profileId: user.profile.id },
     });
+
+    revalidateTag('posts');
 
     return successResponse(post);
   } catch (error) {
     return errorResponse(error, 'Something went wrong creating post.');
+  }
+}
+
+export async function deletePost({ postId }: { postId: string }) {
+  try {
+    const user = await getUser();
+    const post = await prisma.post.findUniqueOrThrow({ where: { id: postId } });
+
+    if (post.profileId !== user.profile.id) {
+      throw new Error('User not authorized to delete post.');
+    }
+
+    const deletedPost = await prisma.post.delete({ where: { id: postId } });
+
+    revalidateTag('posts');
+
+    return successResponse(deletedPost);
+  } catch (error) {
+    return errorResponse(error, 'Something went wrong deleting post.');
   }
 }
