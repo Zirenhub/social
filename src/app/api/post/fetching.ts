89bbc9d2a9f75b7a9e3f844a515110f3ec8761bd
Erdinch Osman <erdinchorhan02@gmail.com>
2025-03-12 02:54:04 +0200

    use unstable_cache when fetching

--- src/app/api/post/fetching.ts
+++ src/app/api/post/fetching.ts
@@ -1,19 +1,24 @@
 import { prisma } from '@/lib/prisma';
 import successResponse, { errorResponse } from '../response';
+import { unstable_cache } from 'next/cache';
 
-export const getPosts = async (filter: string) => {
-  try {
-    const posts = await prisma.post.findMany({
-      include: {
-        profile: true,
-        _count: { select: { likes: true, comments: true } },
-      },
-      orderBy: {
-        createdAt: 'desc',
-      },
-    });
-    return successResponse(posts);
-  } catch (error) {
-    return errorResponse(error, 'Something went wrong fetching posts.');
-  }
-};
+export const getPosts = unstable_cache(
+  async () => {
+    try {
+      const posts = await prisma.post.findMany({
+        include: {
+          profile: true,
+          _count: { select: { likes: true, comments: true } },
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+      });
+      return successResponse(posts);
+    } catch (error) {
+      return errorResponse(error, 'Something went wrong fetching posts.');
+    }
+  },
+  ['posts'], // dynamic tag
+  { tags: ['posts'], revalidate: false } // overall root tag
+);
--- src/app/api/profile/fetching.ts
+++ src/app/api/profile/fetching.ts
@@ -1,16 +1,21 @@
 import { prisma } from '@/lib/prisma';
 import successResponse, { errorResponse } from '../response';
+import { unstable_cache } from 'next/cache';
 
-export const getPostsCount = async (profileId: string) => {
-  try {
-    const count = await prisma.post.count({
-      where: {
-        profileId: profileId,
-      },
-    });
+export const getPostsCount = unstable_cache(
+  async (profileId: string) => {
+    try {
+      const count = await prisma.post.count({
+        where: {
+          profileId: profileId,
+        },
+      });
 
-    return successResponse(count);
-  } catch (error) {
-    return errorResponse(error, 'Failed getting profile posts count.');
-  }
-};
+      return successResponse(count);
+    } catch (error) {
+      return errorResponse(error, 'Failed getting profile posts count.');
+    }
+  },
+  ['posts'],
+  { tags: ['posts'], revalidate: false }
+);
