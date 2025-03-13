abd0888914530fea81e631201d3118133d458d65
Erdinch Osman <erdinchorhan02@gmail.com>
2025-03-13 02:17:29 +0200

    account for full nav width

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -5,7 +5,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex w-full h-screen">
       <Navigation />
-      <main className="flex-1 min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800 pr-12 overflow-auto">
+      <main className="flex-1 min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800 pr-[72px] overflow-auto">
         {children}
       </main>
     </div>
