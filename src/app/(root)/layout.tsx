03c6986531b57b1dbc37a3ceb8815f7839e6ab72
Erdinch Osman <erdinchorhan02@gmail.com>
2025-03-17 03:35:47 +0200

    remove uneeded headers

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,5 +1,4 @@
 import Navigation from '@/components/nav-bar/Navigation';
-import { headers } from 'next/headers';
 import React from 'react';
 import { getUser } from '@/lib/session';
 import LastActiveUpdater from '@/components/LastActiveUpdater';
@@ -10,15 +9,12 @@ export default async function AppLayout({
   children: React.ReactNode;
 }) {
   const user = await getUser();
-  const headersList = await headers();
 
-  // Check if we need to update last active
-  const shouldUpdateLastActive =
-    headersList.get('x-update-last-active') === 'true';
+  // const fillProfileInfoReminder = localStorage.getItem('profile-update');
 
   return (
     <div className="flex w-full h-screen">
-      {shouldUpdateLastActive && <LastActiveUpdater />}
+      <LastActiveUpdater lastActive={user.profile.lastActive} />
       <Navigation user={user} />
       <main className="flex-1 min-h-screen bg-gradient-to-br from-white to-[var(--color-cyan-500)]/5 dark:from-[var(--color-dark-500)] dark:to-[var(--color-purple-500)]/10 pb-12 overflow-auto">
         {children}
