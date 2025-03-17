08bd1db87086a14b27c010ce18a9a70176fbb7c7
Erdinch Osman <erdinchorhan02@gmail.com>
2025-03-17 03:33:50 +0200

    last active status now gets handled here

--- src/components/LastActiveUpdater.tsx
+++ src/components/LastActiveUpdater.tsx
@@ -2,14 +2,31 @@
 
 import { useEffect } from 'react';
 import { updateLastActive } from '@/app/api/auth/actions';
+import { ACTIVITY_THRESHOLDS } from '@/types/constants';
+import { differenceInMinutes, minutesToMilliseconds } from 'date-fns';
 
-export default function LastActiveUpdater() {
+type Props = {
+  lastActive: Date;
+};
+
+export default function LastActiveUpdater({ lastActive }: Props) {
   useEffect(() => {
-    console.log('Updating last active,');
-    async function handleUpdateLastActive() {
-      await updateLastActive();
+    const lastActiveDate = new Date(lastActive);
+    const minutesDifference = differenceInMinutes(new Date(), lastActiveDate);
+    console.log(minutesDifference);
+    if (minutesDifference >= ACTIVITY_THRESHOLDS.UPDATE_LAST_ACTIVE_MINUTES) {
+      updateLastActive();
     }
-    handleUpdateLastActive();
+  }, [lastActive]);
+
+  useEffect(() => {
+    // Set up interval for regular updates
+    const interval = setInterval(() => {
+      updateLastActive();
+    }, minutesToMilliseconds(ACTIVITY_THRESHOLDS.UPDATE_LAST_ACTIVE_MINUTES));
+
+    // Clean up interval on unmount
+    return () => clearInterval(interval);
   }, []);
 
   return null;
