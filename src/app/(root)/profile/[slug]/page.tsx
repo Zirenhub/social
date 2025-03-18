c46b398c5bcf4c5061f703de791f5f87bc510e9a
Erdinch Osman <erdinchorhan02@gmail.com>
2025-03-19 01:03:43 +0200

    conditional isUser checks

--- src/app/(root)/profile/[slug]/page.tsx
+++ src/app/(root)/profile/[slug]/page.tsx
@@ -14,28 +14,37 @@ import MightKnow from '@/components/sidebar/MightKnow';
 import ActivitySummary from '@/components/profile/ActivitySummary';
 import Feed from '@/components/home/Feed';
 import {
-  getProfileBasic,
+  getProfile,
   getProfileLastActive,
+  getProfilePosts,
 } from '@/app/api/profile/fetching';
 import { formatJoinedDate } from '@/helpers/formatDate';
+import { getUser } from '@/lib/session';
+import ErrorParagraph from '@/components/error/ErrorParagraph';
+import ProfileStats from '@/components/profile/ProfileStats';
 
 type Props = { params: Promise<{ slug: string }> };
 
 export default async function Profile({ params }: Props) {
   const { slug } = await params;
-  const profileResult = await getProfileBasic(slug);
+  const profileResult = await getProfile(slug);
 
   if (!profileResult.success || !profileResult.data) {
-    return (
-      <p className="text-center h-full text-lg text-red-400">
-        {profileResult.error?.message}
-      </p>
-    );
+    return <ErrorParagraph message={profileResult.error?.message} />;
   }
 
-  const lastActiveResult = await getProfileLastActive(slug);
+  const profilePosts = await getProfilePosts(slug);
+  const profileLastActive = await getProfileLastActive(slug);
 
-  const result = { ...profileResult.data, lastActive: lastActiveResult };
+  const result = {
+    ...profileResult.data,
+    posts: profilePosts.data,
+    lastActive: profileLastActive,
+  };
+
+  const currentUser = await getUser();
+  const isCurrentUser = currentUser.profile.id === result.id;
+  const { _count } = profileResult.data;
 
   return (
     <div className="mx-14 mt-6">
@@ -54,12 +63,14 @@ export default async function Profile({ params }: Props) {
           >
             <Share2 size={18} className="text-white" />
           </Link>
-          <Link
-            href="/settings"
-            className="flex items-center justify-center w-10 h-10 bg-white/20 hover:bg-white/30 backdrop-blur-md rounded-full transition-all duration-200 hover:scale-105"
-          >
-            <Settings size={18} className="text-white" />
-          </Link>
+          {isCurrentUser && (
+            <Link
+              href="/settings"
+              className="flex items-center justify-center w-10 h-10 bg-white/20 hover:bg-white/30 backdrop-blur-md rounded-full transition-all duration-200 hover:scale-105"
+            >
+              <Settings size={18} className="text-white" />
+            </Link>
+          )}
         </div>
       </div>
 
@@ -68,25 +79,28 @@ export default async function Profile({ params }: Props) {
         <div className="flex gap-6">
           {/* Left side - Profile info */}
           <div className="md:w-2/3">
-            <div className="relative mb-6 bg-white dark:bg-gray-800 rounded-xl shadow-lg border border-gray-200 dark:border-gray-700 hover:shadow-xl transition-all duration-300 ">
+            <div className="relative mb-6 bg-white dark:bg-gray-800 rounded-xl shadow-lg border border-gray-200 dark:border-gray-700 hover:shadow-xl transition-all duration-300">
               {/* Profile picture */}
-              <div className="absolute left-8 -top-6 ring-white dark:ring-[var(--color-dark-500)] rounded-full shadow-xl bg-white p-4">
-                <User2Icon size={120} className="object-cover" color="gray" />
-              </div>
-
-              {/* Edit profile button */}
-              <div className="flex justify-end p-4">
-                <Link
-                  href="/profile/edit"
-                  className="flex items-center gap-2 px-4 py-2 text-sm font-medium transition-all duration-200 rounded-lg bg-[var(--color-cyan-500)]/10 text-[var(--color-cyan-500)] hover:bg-[var(--color-cyan-500)] hover:text-white"
-                >
-                  <Edit size={16} />
-                  Edit Profile
-                </Link>
+              <div className="h-4 mb-28 flex items-start w-full">
+                <div className="absolute left-8 -top-6 ring-white dark:ring-[var(--color-dark-500)] rounded-full shadow-xl bg-white p-4">
+                  <User2Icon size={120} className="object-cover" color="gray" />
+                </div>
+                {/* Edit profile button */}
+                {isCurrentUser && (
+                  <div className="ml-auto">
+                    <Link
+                      href="/profile/edit"
+                      className="flex px-3 py-2 m-3 items-center gap-2 text-sm font-medium transition-all duration-200 rounded-lg bg-[var(--color-cyan-500)]/10 text-[var(--color-cyan-500)] hover:bg-[var(--color-cyan-500)] hover:text-white"
+                    >
+                      <Edit size={16} />
+                      Edit Profile
+                    </Link>
+                  </div>
+                )}
               </div>
 
               {/* Profile content */}
-              <div className="pt-16 pb-8 px-8">
+              <div className="pb-8 px-8">
                 <h1 className="text-3xl font-bold bg-gradient-to-r from-[var(--color-cyan-500)] to-[var(--color-blue-500)] bg-clip-text text-transparent pb-1">
                   {`${result.firstName} ${result.lastName}`}
                 </h1>
@@ -97,7 +111,13 @@ export default async function Profile({ params }: Props) {
 
                 <p className="mt-4 text-[var(--color-dark-500)]/80 dark:text-white/80">
                   {result.bio ||
-                    'No bio yet. Click on Edit Profile to add your bio.'}
+                    (isCurrentUser ? (
+                      'No bio yet. Click on Edit Profile to add your bio.'
+                    ) : (
+                      <span className="text-[var(--color-dark-500)]/30 dark:text-white/80">
+                        This user has no bio.
+                      </span>
+                    ))}
                 </p>
 
                 <div className="flex flex-wrap gap-4 mt-6">
@@ -120,34 +140,11 @@ export default async function Profile({ params }: Props) {
                   </div>
                 </div>
 
-                <div className="flex flex-wrap gap-8 pt-6 mt-8 border-t border-[var(--color-dark-500)]/10 dark:border-white/10">
-                  <div className="text-center">
-                    <div className="text-2xl font-semibold text-[var(--color-dark-500)] dark:text-white">
-                      {0}
-                    </div>
-                    <div className="text-sm text-[var(--color-dark-500)]/60 dark:text-white/60">
-                      Posts
-                    </div>
-                  </div>
-
-                  <div className="text-center">
-                    <div className="text-2xl font-semibold text-[var(--color-dark-500)] dark:text-white">
-                      {0}
-                    </div>
-                    <div className="text-sm text-[var(--color-dark-500)]/60 dark:text-white/60">
-                      Followers
-                    </div>
-                  </div>
-
-                  <div className="text-center">
-                    <div className="text-2xl font-semibold text-[var(--color-dark-500)] dark:text-white">
-                      {0}
-                    </div>
-                    <div className="text-sm text-[var(--color-dark-500)]/60 dark:text-white/60">
-                      Following
-                    </div>
-                  </div>
-                </div>
+                <ProfileStats
+                  postsCount={_count.posts}
+                  followersCount={_count.followers}
+                  followingCount={_count.following}
+                />
               </div>
             </div>
 
@@ -170,7 +167,11 @@ export default async function Profile({ params }: Props) {
             </div>
 
             {/* Posts */}
-            <Feed filter="forYou" />
+            {result.posts === null ? (
+              <ErrorParagraph message={profilePosts.error?.message} />
+            ) : (
+              <Feed posts={result.posts} />
+            )}
           </div>
 
           {/* Right side - Additional info */}
