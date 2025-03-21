26cce7404e38fa60f9b7f9b98f304e9861652309
Erdinch Osman <erdinchorhan02@gmail.com>
2025-03-21 17:33:07 +0200

    use lifted searchbar

--- src/components/home/Sidebar.tsx
+++ src/components/home/Sidebar.tsx
@@ -1,5 +1,5 @@
-import { SearchIcon } from 'lucide-react';
 import MightKnow from '../sidebar/MightKnow';
+import Search from '../sidebar/Search';
 
 export default function Sidebar() {
   // Sample trending topics
@@ -14,18 +14,7 @@ export default function Sidebar() {
   return (
     <div className="overflow-auto flex flex-col gap-3 no-scrollbar">
       {/* Search Box */}
-      <div className="bg-white dark:bg-gray-800 rounded-xl shadow-sm p-4 border border-gray-200 dark:border-gray-700">
-        <div className="relative">
-          <input
-            type="text"
-            placeholder="Search..."
-            className="w-full pl-10 pr-4 py-2 bg-gray-100 dark:bg-gray-700 border-0 rounded-lg focus:ring-2 focus:ring-[var(--color-cyan-500)] focus:bg-white dark:focus:bg-gray-800 transition-all duration-200"
-          />
-          <div className="w-5 h-5 text-gray-400 absolute left-3 top-2.5">
-            <SearchIcon />
-          </div>
-        </div>
-      </div>
+      <Search />
 
       {/* Trending Topics */}
       <div className="bg-white dark:bg-gray-800 rounded-xl shadow-sm p-4 border border-gray-200 dark:border-gray-700">
