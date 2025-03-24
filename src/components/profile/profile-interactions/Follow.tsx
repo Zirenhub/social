7f05cb8b00e5f702275eeaf12cccea0dacf1dac8
Erdinch Osman <erdinchorhan02@gmail.com>
2025-03-25 00:16:43 +0200

    follow button prop styling, side effect

--- src/components/profile/profile-interactions/Follow.tsx
+++ src/components/profile/profile-interactions/Follow.tsx
@@ -1,14 +1,17 @@
 'use client';
 import { followProfile, unfollowProfile } from '@/app/api/profile/actions';
 import { GetProfileType } from '@/types/profile';
+import { UserRoundMinusIcon, UserRoundPlusIcon } from 'lucide-react';
 import { useState } from 'react';
 import { toast } from 'react-toastify';
 
 type Props = {
   profile: GetProfileType;
+  sideEffect?: () => void;
+  className?: string;
 };
 
-export default function Follow({ profile }: Props) {
+export default function Follow({ sideEffect, profile, className }: Props) {
   const [isLoading, setIsLoading] = useState(false);
   const [isFollowing, setIsFollowing] = useState(profile.followers.length > 0);
   const isFollowingMe = profile.following.length > 0;
@@ -19,16 +22,23 @@ export default function Follow({ profile }: Props) {
 
       if (isFollowing) {
         await unfollowProfile(profile.id);
-        toast.success('Unfollowed!');
+        toast.success(`Unfollowed @${profile.username}`);
         setIsFollowing(false);
       } else {
         await followProfile(profile.id);
-        toast.success(isFollowingMe ? 'Followed back!' : 'Followed!');
+        toast.success(
+          isFollowingMe
+            ? `Followed back @${profile.username}`
+            : `Followed @${profile.username}`
+        );
         setIsFollowing(true);
       }
     } catch (error) {
       toast.error('Action failed. Please try again.');
     } finally {
+      if (sideEffect) {
+        sideEffect();
+      }
       setIsLoading(false);
     }
   };
@@ -46,19 +56,31 @@ export default function Follow({ profile }: Props) {
 
   const buttonText = getButtonText();
 
+  const getClassName = () => {
+    if (className) {
+      return className;
+    }
+
+    return 'primary-button';
+  };
+
   return (
     <button
-      className={`${isFollowing ? 'primary-button group' : 'primary-button'}`}
+      className={`${isFollowing ? `${getClassName()} group` : getClassName()}`}
       disabled={isLoading}
       onClick={handleFollowAction}
     >
       {isFollowing ? (
         <>
-          <span className="hidden group-hover:block">{buttonText.hover}</span>
+          <span className="hidden group-hover:flex items-center">
+            <UserRoundMinusIcon size={16} className="mr-2" /> {buttonText.hover}
+          </span>
           <span className="group-hover:hidden">{buttonText.default}</span>
         </>
       ) : (
-        buttonText.default
+        <div className="flex items-center">
+          <UserRoundPlusIcon size={16} className="mr-2" /> {buttonText.default}
+        </div>
       )}
     </button>
   );
