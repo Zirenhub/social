056ef4d4eaf01595c428f4ce5a4a9ca179f16e2f
Erdinch Osman <erdinchorhan02@gmail.com>
2025-03-25 02:13:31 +0200

    following filter for home posts

--- src/app/api/post/fetching.ts
+++ src/app/api/post/fetching.ts
@@ -1,24 +1,48 @@
 import { prisma } from '@/lib/prisma';
 import successResponse, { errorResponse } from '../response';
 import { unstable_cache } from 'next/cache';
-import { CACHE_TAGS } from '@/types/constants';
-import { postQuery } from '@/types/post';
+import { CACHE_TAGS, HomePagePostsFilter } from '@/types/constants';
+import { postQuery, PostWithCounts } from '@/types/post';
+import { getUser } from '@/lib/session';
+import { getProfile } from '../profile/fetching';
 
-type getPostsProps = {
-  filter: string;
-};
+export const getHomePosts = async ({
+  filter,
+}: {
+  filter: HomePagePostsFilter;
+}) => {
+  const user = await getUser();
 
-export const getHomePosts = ({ filter }: getPostsProps) => {
   return unstable_cache(
     async () => {
       try {
-        const posts = await prisma.post.findMany(postQuery({}));
+        let posts: PostWithCounts[] = [];
+
+        if (filter === 'forYou') {
+          const forYouPosts = await prisma.post.findMany({ ...postQuery({}) });
+          posts = forYouPosts;
+        }
+
+        if (filter === 'following') {
+          const profile = await prisma.profile.findUniqueOrThrow({
+            where: { id: user.profile.id },
+            include: {
+              following: {
+                include: { following: { select: { posts: postQuery({}) } } },
+              },
+            },
+          });
+
+          for (const follower of profile.following) {
+            posts.push(...follower.following.posts);
+          }
+        }
         return successResponse(posts);
       } catch (error) {
         return errorResponse(error, 'Something went wrong fetching posts.');
       }
     },
-    [CACHE_TAGS.POSTS],
-    { tags: [CACHE_TAGS.POSTS] }
+    [CACHE_TAGS.POSTS(filter)],
+    { tags: [CACHE_TAGS.POSTS(filter)] }
   )();
 };
