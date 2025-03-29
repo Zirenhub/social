48cf271d8727dd4be4e65135e4434d1ce9244a6c
Erdinch Osman <erdinchorhan02@gmail.com>
2025-03-29 02:54:34 +0200

    fetch profile on post container

--- src/components/post/PostContainer.tsx
+++ src/components/post/PostContainer.tsx
@@ -1,31 +1,38 @@
-'use client';
 import { PostWithCounts } from '@/types/post';
 import PostHeader from './PostHeader';
 import { formatCreatedAtDate } from '@/helpers/formatDate';
 import PostInteractions from './PostInteractions';
-import { useState } from 'react';
-import { GetProfileType } from '@/types/profile';
+import { getProfile } from '@/app/api/profile/fetching';
+import PostOptions from './PostOptions';
+import getSession from '@/lib/getSession';
 
 type Props = {
   post: PostWithCounts;
 };
 
-export default function PostContainer({ post }: Props) {
-  const [postProfile, setPostProfile] = useState<GetProfileType | null>(null);
+export default async function PostContainer({ post }: Props) {
+  const session = await getSession();
+  const profile = await getProfile({
+    profileId: post.profileId,
+    userProfileId: session.user.profile,
+  });
 
   return (
     <div
       key={post.id}
       className="bg-white dark:bg-gray-800 rounded-xl shadow-sm p-4 mb-4 border border-gray-200 dark:border-gray-700 hover:border-gray-300 dark:hover:border-gray-600 transition-all duration-200"
     >
-      <PostHeader
-        profile={{
-          firstName: post.profile.firstName,
-          lastName: post.profile.lastName,
-          id: post.profile.id,
-        }}
-        createdAt={formatCreatedAtDate(post.createdAt)}
-      />
+      <div className="flex justify-between items-start">
+        <PostHeader
+          profile={profile}
+          createdAt={formatCreatedAtDate(post.createdAt)}
+        />
+        <PostOptions
+          post={post}
+          profile={profile}
+          isOwner={post.profileId === session.user.profile}
+        />
+      </div>
       <p className="text-gray-800 dark:text-gray-200 mb-4">{post.content}</p>
       <PostInteractions post={post} />
     </div>
