b361638325e928cce0666f80f5d5759da7d8a46f
Erdinch Osman <erdinchorhan02@gmail.com>
2025-03-29 02:48:49 +0200

    fixed config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,12 +10,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier', {
+  compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  {
     rules: {
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
-    }, // REMOVE THESE LATER
-  }),
+    },
+  },
 ];
 
 export default eslintConfig;
