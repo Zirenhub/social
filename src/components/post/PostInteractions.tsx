7e445698095fb12b3d2e87f9d7603958f84e37ff
Erdinch Osman <erdinchorhan02@gmail.com>
2025-03-30 12:10:26 +0300

    like post lifted to hook

--- src/components/post/PostInteractions.tsx
+++ src/components/post/PostInteractions.tsx
@@ -1,17 +1,20 @@
 'use client';
-import { likePost } from '@/app/api/post/actions';
+import useLike from '@/hooks/post/useLike';
 import { PostWithCounts } from '@/types/post';
 import { HeartIcon, MessageCircleMoreIcon, RepeatIcon } from 'lucide-react';
-import { useState } from 'react';
 import { toast } from 'react-toastify';
+import LoaderPlaceholder from '../ui/LoaderPlaceholder';
 
 type Props = {
   post: PostWithCounts;
 };
 
 export default function PostInteractions({ post }: Props) {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const postIsLiked = post.likes.length > 0;
+  const {
+    postIsLiked,
+    handleLike,
+    isPending: likeIsPending,
+  } = useLike({ post });
 
   const interactions = [
     {
@@ -24,29 +27,22 @@ export default function PostInteractions({ post }: Props) {
         />
       ),
       count: post._count.likes,
-      noClick: async () => {
-        setIsLoading(true);
-        const result = await likePost({ postId: post.id });
-        if (!result.success || !result.data) {
-          toast.error(
-            `Something went wrong,
-            ${result.error?.message || 'unknown'}`
-          );
-        }
-        setIsLoading(false);
-      },
+      noClick: handleLike,
+      isPending: likeIsPending,
     },
     {
       label: 'Comments',
       icon: <MessageCircleMoreIcon size={21} />,
       count: post._count.comments,
       noClick: () => toast.success(`Opening comments on post ${post.id}`),
+      isPending: false,
     },
     {
       label: 'Share',
       icon: <RepeatIcon size={21} />,
       count: 0,
       noClick: () => toast.success(`Repost post ${post.id}`),
+      isPending: false,
     },
   ];
 
@@ -55,13 +51,13 @@ export default function PostInteractions({ post }: Props) {
       {interactions.map((interaction) => {
         return (
           <button
-            disabled={isLoading}
+            disabled={interaction.isPending}
             key={interaction.label}
             onClick={interaction.noClick}
             className="cursor-pointer flex items-center gap-1 hover:text-[var(--color-cyan-500)] transition-colors h-9"
           >
             <span className="h-[21px] flex items-center">
-              {interaction.icon}
+              {interaction.isPending ? <LoaderPlaceholder /> : interaction.icon}
             </span>
             <span className="min-w-[1ch] text-center mt-1">
               {interaction.count}
---
+++ src/hooks/post/useLike.ts
@@ -0,0 +1,30 @@
+'use client';
+
+import { likePost } from '@/app/api/post/actions';
+import { PostWithCounts } from '@/types/post';
+import { useTransition } from 'react';
+import { toast } from 'react-toastify';
+
+type Props = {
+  post: PostWithCounts;
+};
+
+export default function useLike({ post }: Props) {
+  const [isPending, startTransition] = useTransition();
+
+  const postIsLiked = post.likes.length > 0;
+
+  const handleLike = () => {
+    startTransition(async () => {
+      const result = await likePost({ postId: post.id });
+      if (!result.success || !result.data) {
+        toast.error(
+          `Something went wrong,
+                ${result.error?.message || 'unknown'}`
+        );
+      }
+    });
+  };
+
+  return { isPending, handleLike, postIsLiked };
+}
