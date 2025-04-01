82c3a8f7353a1e346ec059b96595a0d3e85d2f2a
Erdinch Osman <erdinchorhan02@gmail.com>
2025-04-02 02:41:13 +0300

    naming change, dont use server for get

--- src/app/api/auth/fetching.ts
+++ src/app/api/auth/fetching.ts
@@ -1,13 +1,8 @@
-'use server';
 import { prisma } from '@/lib/prisma';
 import { errorResponse } from '../response';
 import { GetUserType } from '@/types/auth';
-import { unstable_cacheTag as cacheTag } from 'next/cache';
-import { CACHE_TAGS } from '@/types/constants';
 
-export const GetUser = async (userId: string): Promise<GetUserType> => {
-  'use cache';
-  cacheTag(CACHE_TAGS.USER(userId));
+export const getUser = async (userId: string): Promise<GetUserType> => {
   try {
     const user = await prisma.user.findFirst({
       where: {
@@ -23,6 +18,6 @@ export const GetUser = async (userId: string): Promise<GetUserType> => {
     return { ...safeUser, profile };
   } catch (error) {
     const err = errorResponse(error, 'Something went wrong getting user');
-    throw new Error(err.error?.message);
+    throw new Error(err.error.message);
   }
 };
