ab414892547708d23918e7713aeafed96377db04
Erdinch Osman <erdinchorhan02@gmail.com>
2025-04-02 02:18:25 +0300

    date could be undefined

--- src/helpers/formatDate.ts
+++ src/helpers/formatDate.ts
@@ -1,6 +1,8 @@
 import { formatDistance } from 'date-fns';
 
-export function formatJoinedDate(createdAt: Date) {
+export function formatJoinedDate(createdAt: Date | undefined) {
+  if (!createdAt) return null;
+
   return new Date(createdAt).toLocaleDateString('en-US', {
     month: 'long',
     year: 'numeric',
