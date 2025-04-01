82753d2ac72545befdd24833f0ea61e7131818d8
Erdinch Osman <erdinchorhan02@gmail.com>
2025-04-02 02:19:24 +0300

    use class if provided

--- src/components/ui/LoaderPlaceholder.tsx
+++ src/components/ui/LoaderPlaceholder.tsx
@@ -1,13 +1,23 @@
 import { Loader2 } from 'lucide-react';
 
-type Props = { size?: number; text?: string; color?: string };
+type Props = {
+  size?: number;
+  text?: string;
+  color?: string;
+  className?: string;
+};
 
-export default function LoaderPlaceholder({ size, text, color }: Props) {
+export default function LoaderPlaceholder({
+  size,
+  text,
+  color,
+  className,
+}: Props) {
   return (
     <>
       <Loader2
         size={size || 24}
-        className="animate-spin"
+        className={`animate-spin ${className} `}
         color={color || 'gray'}
       />
       {text && <p className="text-gray-500">{text}</p>}
