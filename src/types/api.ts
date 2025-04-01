4744158deaa86b5bdc9cb279c93bf2442ff3eb84
Erdinch Osman <erdinchorhan02@gmail.com>
2025-04-02 02:16:57 +0300

    readable code

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,9 +1,11 @@
-export type ApiResponse<T> =
-  | { success: true; data: T; error: null }
-  | { success: false; data: null; error: ApiError };
-
 // Unified error type
-export type ApiError = {
+export type ApiErrorType = {
   message: string;
   fields?: Record<string, string>; // Optional field-specific errors
 };
+
+type ApiSuccess<T> = { success: true; data: T; error: null };
+
+export type ApiError = { success: false; data: null; error: ApiErrorType };
+
+export type ApiResponse<T> = ApiSuccess<T> | ApiError;
