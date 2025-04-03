90823ff7043b9f5b0e64f6af788b0824c5894fe4
Erdinch Osman <erdinchorhan02@gmail.com>
2025-04-03 17:26:44 +0300

    query client

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,10 +1,13 @@
 'use client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'next-themes';
+
+const queryClient = new QueryClient();
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
-      {children}
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </ThemeProvider>
   );
 }
