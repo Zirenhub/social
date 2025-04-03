3527e37257225badb3c885f45ab895797e9240b7
Erdinch Osman <erdinchorhan02@gmail.com>
2025-04-03 17:27:52 +0300

    per page

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,9 +1,9 @@
 import { type NextRequest, NextResponse } from 'next/server';
 import { getHomePosts } from '@/app/api/posts/fetching';
 import getSession from '@/lib/getSession';
-import { HomePagePostsFilter, PER_PAGE } from '@/types/constants';
-import { ApiResponse } from '@/types/api';
-import { PaginatedPosts } from '@/types/post';
+import { type HomePagePostsFilter, PER_PAGE } from '@/types/constants';
+import type { ApiResponse } from '@/types/api';
+import type { PaginatedPosts } from '@/types/post';
 import successResponse, { errorResponse } from '../response';
 
 export async function GET(
@@ -14,18 +14,23 @@ export async function GET(
     const filter =
       (searchParams.get('filter') as HomePagePostsFilter) || 'forYou';
     const page = Number.parseInt(searchParams.get('page') || '1', 10);
+    const perPage = Number.parseInt(
+      searchParams.get('perPage') || String(PER_PAGE),
+      10
+    );
 
     const session = await getSession();
 
     const paginatedPosts = await getHomePosts({
       filter,
       userProfileId: session.user.profile,
-      perPage: PER_PAGE,
+      perPage,
       page,
     });
 
     return NextResponse.json(successResponse(paginatedPosts));
   } catch (error) {
+    console.error('API Error:', error);
     return NextResponse.json(errorResponse(error));
   }
 }
