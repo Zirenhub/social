309e9409f2097197537a961b0d693b0ec98bb558
Erdinch Osman <erdinchorhan02@gmail.com>
2025-04-06 15:06:02 +0300

    search route

---
+++ src/app/api/search/route.ts
@@ -0,0 +1,15 @@
+import { getSearchProfiles } from '@/app/api/search/fetching';
+import { NextRequest, NextResponse } from 'next/server';
+import successResponse, { errorResponse } from '../response';
+
+export async function GET(req: NextRequest) {
+  const query = req.nextUrl.searchParams.get('query');
+  if (!query) return NextResponse.json([]);
+
+  try {
+    const results = await getSearchProfiles(query);
+    return NextResponse.json(successResponse(results));
+  } catch (error) {
+    return NextResponse.json(errorResponse(error));
+  }
+}
