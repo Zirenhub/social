b884dc67a9a9d87fd341bcd724cf36c45622fe9e
Erdinch Osman <erdinchorhan02@gmail.com>
2025-04-07 00:50:56 +0300

    react query dev tools, init query client properly

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,19 @@
 'use client';
+
+import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from 'next-themes';
 
-const queryClient = new QueryClient();
-
 export default function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
