38094ab7a589c7de21f7a2c72cd4ef8dd6c9ae4c
Erdinch Osman <erdinchorhan02@gmail.com>
2025-04-07 20:42:08 +0300

    dont refetch on window focus

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -6,7 +6,16 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from 'next-themes';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  );
 
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
